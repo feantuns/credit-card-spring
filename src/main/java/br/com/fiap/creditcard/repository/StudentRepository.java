32461ff6683dcf06e19afe30fd51234a5828f950
Felipe Antunes <felipeantunes542@gmail.com>
2023-03-02 21:15:51 -0300

    like search student by name

--- src/main/java/br/com/fiap/creditcard/repository/StudentRepository.java
+++ src/main/java/br/com/fiap/creditcard/repository/StudentRepository.java
@@ -3,12 +3,13 @@ package br.com.fiap.creditcard.repository;
 import br.com.fiap.creditcard.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
 
-    @Query("select s from StudentEntity s where s.name LIKE :name")
-    List<StudentEntity> findAllByName(String name);
+    @Query("select s from StudentEntity s where s.name LIKE %:name%")
+    List<StudentEntity> findAllByName(@Param("name") String name);
 
 }
